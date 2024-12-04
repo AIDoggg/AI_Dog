2ec0acf097f187eb568fd1024fa17975e8865bb1
AIDoggg <c99496862@gmail.com>
2024-12-04 09:32:15 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Tokenomics from "../components/Tokenomics";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Tokenomics />
+      <Footer />
+    </>
+  );
+}
