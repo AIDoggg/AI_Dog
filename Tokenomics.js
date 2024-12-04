30350218b9d2aa41ec4d9cccc907539807d67672
AIDoggg <c99496862@gmail.com>
2024-12-04 09:32:00 -0500

    Create Tokenomics.js

---
+++ Tokenomics.js
@@ -0,0 +1,15 @@
+export default function Tokenomics() {
+  return (
+    <section id="tokenomics" className="p-8">
+      <h2 className="text-3xl font-bold text-center">Tokenomics</h2>
+      <ul className="mt-4">
+        <li>Supply: 1,000,000,000 WOOF</li>
+        <li>50% Community Rewards</li>
+        <li>20% Liquidity</li>
+        <li>15% Development Fund</li>
+        <li>10% Marketing</li>
+        <li>5% Founders’ Reserve</li>
+      </ul>
+    </section>
+  );
+}
